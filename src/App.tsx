de01cec1628a95a32906fc777af848a20802c142
quoccuong021099 <quoccuong021099@gmail.com>
2021-12-08 17:18:21 +0700

    u

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import cityApi from 'api/cityApi';
+import studentApi from 'api/studentApi';
+import React, { useEffect } from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+
+  useEffect(() => {
+    cityApi.getAll().then(res => console.log(res.data.map(x => x.name)));
+    studentApi.getAll({
+      _page: 1,
+      _limit: 10,
+      _sort: 'name',
+      _order: 'asc',
+    }).then(res => console.log(res.data));
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div >
+      <Routes>
+        <Route path="/" element={<>a</>}>
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+            <Route path="new" element={<NewTeamForm />} />
+            <Route index element={<LeagueStandings />} />
+          </Route> */}
+        </Route>
+      </Routes>
     </div>
   );
 }
